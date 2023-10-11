1d4ccf2cb7be8deee8f6db8f8248b51931d04a2b
Ashutosh <ashutoshkhairnarofficial@gmail.com>
2023-10-11 10:14:12 +0530

    commit

---
+++ DayEighteen/src/com/tnsif/generic_class/ArrayOperations.java
@@ -0,0 +1,16 @@
+package com.tnsif.generic_class;
+
+import java.util.Arrays;
+
+public class ArrayOperations {
+	public static <E> void display(E values[]) {
+		for (E ele : values) {
+			System.out.print(ele + " ");
+		}
+		System.out.println();
+	}
+
+	public static <E> void sortArray(E values[]) {
+		Arrays.sort(values);
+	}
+}
---
+++ DayEighteen/src/com/tnsif/generic_class/Repo.java
@@ -0,0 +1,14 @@
+package com.tnsif.generic_class;
+
+public class Repo<T> {
+		T data;
+
+		public T getData() {
+			return data;
+		}
+
+		public void setData(T data) {
+			this.data = data;
+		}
+		
+}
