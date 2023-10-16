f40c80610df830e606669809b83ac2ea5ea96321
Ashutosh <ashutoshkhairnarofficial@gmail.com>
2023-10-16 13:11:25 +0530

    Commit

---
+++ DayTwentyOne/src/org/tnsif/twentyone/Cube.java
@@ -0,0 +1,5 @@
+package org.tnsif.twentyone;
+
+public interface Cube {
+	int getCube(int no);
+}
---
+++ DayTwentyOne/src/org/tnsif/twentyone/Operations.java
@@ -0,0 +1,5 @@
+package org.tnsif.twentyone;
+
+public interface Operations {
+	int performArithmetic(int a,int b);
+}
