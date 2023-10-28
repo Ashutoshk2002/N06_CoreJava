9ed4a1fa8ca4cd5303019f333f23a27a29c916e9
Ashutosh <ashutoshkhairnarofficial@gmail.com>
2023-10-28 13:02:11 +0530

    commit

---
+++ HIbernate/src/com/tnsif/jpaintro/inheritance/classpertable/Car.java
@@ -0,0 +1,37 @@
+package com.tnsif.jpaintro.inheritance.classpertable;
+
+public class Car extends Vehicle {
+	private float average;
+	private String fuelType;
+	private float price;
+
+	public float getAverage() {
+		return average;
+	}
+
+	public void setAverage(float average) {
+		this.average = average;
+	}
+
+	public String getFuelType() {
+		return fuelType;
+	}
+
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [average=" + average + ", fuelType=" + fuelType + ", price=" + price + "]";
+	}
+
+}
---
+++ HIbernate/src/com/tnsif/jpaintro/inheritance/classpertable/Executor.java
@@ -0,0 +1,37 @@
+package com.tnsif.jpaintro.inheritance.classpertable;
+
+import javax.persistence.EntityManager;
+
+import com.tnsif.hibernate.JPAIUtil;
+
+public class Executor {
+	public static void main(String[] args) {
+		EntityManager em=JPAIUtil.getEntityManager();
+		
+		//add vehicle record
+		Vehicle vobj=new Vehicle();
+		vobj.setType("Four Wheeler");
+		vobj.setModelNo(1234);
+		vobj.setCompany("Ford");
+		
+		Vehicle vobjOne=new Vehicle();
+		vobjOne.setType("Four Wheeler");
+		vobjOne.setModelNo(4321);
+		vobjOne.setCompany("Mahindra");
+		
+		Car carObjOne=new Car();
+		carObjOne.setModelNo(222);
+		carObjOne.setCompany("Honda");
+		carObjOne.setFuelType("Petrol");
+		carObjOne.setAverage(17);
+		carObjOne.setPrice(100000);
+		carObjOne.setType("Four Wheeler");
+		
+		em.getTransaction().begin();
+		em.persist(vobj);
+		em.persist(vobjOne);
+		em.persist(carObjOne);
+		em.getTransaction().commit();
+		
+	}
+}
